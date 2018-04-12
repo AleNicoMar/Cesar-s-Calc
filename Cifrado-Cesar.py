b4dc68fa2451f4b2cf1f7e2210c649aa5612f872
AleNicoMar <alenicomar99@gmail.com>
2018-04-12 20:48:54 -0300

    Add files via upload

--- Cifrado-Cesar.py
+++ Cifrado-Cesar.py
@@ -1,7 +1,16 @@
 # -*- coding: utf-8 -*-
 
 # Importo el módulo 'os' que, en este caso, contiene funciones del SO tales como 'cls' o 'pause>nul'
-import os
+import time
+from os import system
+import sys
+
+#Verifica el SO para utilizar el comando del sistema
+def clear():
+	if(sys.platform == "win32" or sys.platform == "win64"):
+		system('cls')
+	elif(sys.platform == "linux2" or sys.platform == "linux"):
+		system('clear')
 
 # Declaro los abecedarios a utilizar.
 alf = "abcdefghijklmnñopqrstuvwxyz"
@@ -12,24 +21,24 @@ def main():
 	while (loop == True):
 		opcion = input("¿Qué es lo que quiere hacer?\n > ")
 		if (opcion == "cifrar" or opcion == "c"):
-			os.system('cls')
+			clear()
 			plano = input("Inserte el texto plano:\n > ").lower()
 			llave = input("Inserte el desplazamiento:\n > ")
-			os.system('cls')
+			clear()
 			f = open('MensajeCif.txt', 'w')		# Abro el archivo "MensajeCif.txt"(si no existe se crea) en modo escritura y lo guardo en la variable 'f'.
 			f.write(cifrar(plano, llave))		# Escribo el resultado de la función cifrar en el archivo "MensajeCif.txt".
 			print("El mensaje ha sido cifrado satisfactoriamente.")
-			os.system('pause>nul')
+			input('Esperando a que presione una tecla...')
 
 		elif (opcion == "decifrar" or opcion == "d"):
-			os.system('cls')
+			clear()
 			key = input("Inserte el desplazamiento:\n > ")
-			os.system('cls')
+			clear()
 			l = open('MensajeCif.txt', 'r')		# Abro el archivo "MensajeCif.txt"(si no existe, tira error) en modo lectura y lo guardo en la variable 'l'.
 			f = open('MensajeDec.txt', 'w')		# Abro el archivo "MensajeDec.txt"(si no existe, tira error) en modo lectura y lo guardo en la variable 'f'.
 			f.write(decifrar(l.read(), key))	# Escribo el resultado de la función decifrar en el archivo "MensajeDec.txt".
 			print("El mensaje ha sido decifrado satisfactoriamente.")
-			os.system('pause>nul')
+			input('Esperando a que presione una tecla...')
 
 		elif (opcion == "salir" or opcion == "s"):
 			loop = False
@@ -45,8 +54,8 @@ def cifrar(texto, llave):
 		modulo = int(suma) % len(alf)
 		cifrado = cifrado + str(alf[modulo])
 		print(cifrado)
-		os.system('sleep(1000)')
-		os.system('cls')
+		time.sleep(0.1)
+		clear()
 	return cifrado.upper() # Devuelve todo en mayúsculas.
 
 # La función cifrar realiza la operación inversa que también está en Wikipedia.
@@ -57,8 +66,8 @@ def decifrar(texto, llave):
 		modulo = int(resta) % len(alf2)
 		cifrado = cifrado + str(alf2[modulo])
 		print(cifrado)
-		os.system('sleep(1000)')
-		os.system('cls')
+		time.sleep(0.1)
+		clear()
 	return cifrado.upper()
 
 # Llamo a la función main, es decir, da inicio al programa
